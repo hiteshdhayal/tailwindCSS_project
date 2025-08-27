1465119cecb0ba40e392a2e6230ac0f55e45d245
hitesh dhayal <hiteshdhayal@gmail.com>
2025-08-28 00:55:31 +0530

    just adding animation

---
+++ script.js
@@ -0,0 +1,10 @@
+ const text = "your own UI library";
+    let i = 0;
+    function typeWriter() {
+      if (i < text.length) {
+        document.getElementById("my-heading").textContent += text.charAt(i);
+        i++;
+        setTimeout(typeWriter, 100); // speed in ms
+      }
+    }
+    typeWriter();
